a7f377753dac0e8aa11b3866c112ba50f4859e43
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-18 05:42:12 +1200

    updatedwebdriverversion

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ def get_driver():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("window-size=1024,768")
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     return driver
 
