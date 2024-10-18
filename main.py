3f2ffb8dbe574eb6d00ce2900295e9350ae2a836
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-19 01:49:53 +1200

    updatedwebdriverversion

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ SPM_APIKEY = os.getenv('SPM_APIKEY')
 
 def get_driver():
     chrome_options = Options()
+     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--no-sandbox')
+   
     # chrome_options.add_argument('--headless')
     # chrome_options.add_argument("--disable-gpu")
     # chrome_options.add_argument("window-size=1024,768")
