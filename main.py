3cddbf10bc1b2805ef121ad421d6926070f55efd
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-19 01:08:26 +1200

    updatedwebdriverversion

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ SPM_APIKEY = os.getenv('SPM_APIKEY')
 
 def get_driver():
     chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("window-size=1024,768")
-    chrome_options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("window-size=1024,768")
+    # chrome_options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
 
     # Initialize the Chrome driver with Zyte SmartProxy options
     driver = zyte_webdriver.Chrome(
