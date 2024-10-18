9332b87421fccce8995f02454aef311b0f134ae4
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-19 01:48:07 +1200

    updatedwebdriverversion

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ SPM_APIKEY = os.getenv('SPM_APIKEY')
 
 def get_driver():
     chrome_options = Options()
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--no-sandbox')
     # chrome_options.add_argument('--headless')
