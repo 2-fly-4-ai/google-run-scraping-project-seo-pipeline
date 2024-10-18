f9b968f9b07fe285f173a552158f47c1ae9f7859
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-19 11:13:15 +1200

    updatedwebdriverversion

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def wait_for_download_link(driver, timeout=60):
 def get_driver():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(
