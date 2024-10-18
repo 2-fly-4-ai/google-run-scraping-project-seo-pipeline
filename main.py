e8a3bc6c7c6bf524b17540f490a7a69e8ec5e0fd
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-19 06:53:01 +1200

    updatedwebdriverversion

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ def get_driver():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(
         options=chrome_options,
