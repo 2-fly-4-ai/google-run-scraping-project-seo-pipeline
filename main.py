c6178ed9fbcfbd8f9ca46fbb5fd439d21cc9c73b
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-19 06:49:10 +1200

    updatedwebdriverversion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-import seleniumwire.undetected_chromedriver as uc
+from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -46,7 +46,7 @@ def get_driver():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = uc.Chrome(
+    driver = webdriver.Chrome(
         options=chrome_options,
         seleniumwire_options=proxy_options
     )
