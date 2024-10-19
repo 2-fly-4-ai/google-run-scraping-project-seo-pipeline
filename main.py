d335199513360a1b17d686ec3f60329187ab7cb6
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-19 12:43:40 +1200

    updatedwebdriverversion

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def get_driver():
     
     chrome_options.add_argument('--headless')
     chrome_options.add_argument("--disable-blink-features=AutomationControlled") 
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+
     chrome_options.add_experimental_option("useAutomationExtension", False) 
     chrome_options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
     chrome_options.add_argument('--disable-dev-shm-usage')
