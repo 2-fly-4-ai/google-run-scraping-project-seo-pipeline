0703b435ee67d48bb29624c012880dd02519b0fd
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-19 14:40:47 +1200

    updatedwebdriverversion

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def wait_for_download_link(driver, timeout=60):
 
 def get_driver():
     chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless') 
     chrome_options.add_argument('--disable-dev-shm-usage')
     capabilities = chrome_options.to_capabilities()         #cap
