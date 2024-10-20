3d4ef7b74efe305b3b85da9587a536bdcc2ff5d6
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-21 00:50:16 +1200

    changed code to html scraper

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ proxy_options = {
 
 def get_driver():
     chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(
         options=chrome_options,
-      
         seleniumwire_options=proxy_options,
     )
 
