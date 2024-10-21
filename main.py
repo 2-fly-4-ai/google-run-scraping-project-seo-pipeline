0be58232de2de3e2aaf06fe0899dfeaf70a72274
2-fly-4-ai <farleythecoder@gmail.com>
2024-10-21 18:19:42 +1200

    Added LLM element

--- main.py
+++ main.py
@@ -132,13 +132,13 @@ def scrape_html():
         driver.get(url)
         # cf_manual_solver(driver)
 
-        wait = WebDriverWait(driver, timeout=2)
+        # wait = WebDriverWait(driver, timeout=2)
 
-        wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
-          # LETS IMPLEMENT THIS LOGIC. If the website is a redit url. Then we shoud add this 
+        # wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
+        #   # LETS IMPLEMENT THIS LOGIC. If the website is a redit url. Then we shoud add this 
 
-        if "reddit.com" in url:
-            wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@data-testid='post-comment-header']/parent::div")))
+        # if "reddit.com" in url:
+        #     wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@data-testid='post-comment-header']/parent::div")))
 
         driver.execute_script("window.scrollTo(0, 600);")
         html_content = driver.page_source
